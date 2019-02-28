c6aa3399affd88061e24955742fc8289e9166de6
shawn li <shawnl9025@gmail.com>
2019-02-27 20:14:35 -0500

    moved wiki call to server

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ import json
 import random
 import math
 import preprocessor
+from bs4 import BeautifulSoup
+import requests
+import re
 
 app = Flask(__name__)
 
@@ -17,8 +20,8 @@ rare_percentage = 0.1
     return
 
     {
-        "start": year
-        "stop": year
+        start: year
+        stop: year
     }
     years are inclusive
 """
@@ -37,16 +40,17 @@ type_options = ['random', 'common', 'rare']
     POST
 
     {
-        "type":
-        "sex":
-        "year": 
+        type
+        sex
+        year 
             optional
     }
 
     return
 
     {
-        "name":
+        name
+        description
     }
 
 """
@@ -73,23 +77,72 @@ def name():
 
     last_index = len(df_to_use) - 1
 
-    # write response
+    # determine name
     if name_type == 'random':
         selected_row = df_to_use.iloc[random.randint(0, last_index)]
 
-        return jsonify({
-            'name': selected_row['name']
-        })
+        name = selected_row['name']
     else:
         if name_type == 'common':
             selected_row = df_to_use.iloc[random.randint(0, math.ceil(0.1 * last_index))]
         elif name_type == 'rare':
             selected_row = df_to_use.iloc[random.randint(math.floor(0.9 * last_index), last_index)]
 
-        return jsonify({
-            'name': selected_row['name']
-        })
+        name = selected_row['name']
 
+    # get description
+
+
+    return jsonify({
+        'name': name,
+        'description': get_name_description(name),
+    })
+
+
+wikiEndpoint = 'https://en.wikipedia.org/wiki/'
+wikiURLPatterns = ['<Name>_(given_name)', '<Name>_(name)', '<Name>',]
+
+
+def get_name_description(name):
+    # capitalize first letter
+    name = name.lower()
+    name = name[0].upper() + name[1:]
+
+    for pattern in wikiURLPatterns:
+        response = requests.get(wikiEndpoint + re.sub('<Name>', name, pattern))
+        if not response.ok:
+            continue
+        
+        soup = BeautifulSoup(response.text, features="html.parser")
+        main_content = soup.select('.mw-parser-output')[0]
+
+        paragraph_counter = 0
+        text = ''
+
+        for child in main_content.children:
+            if child.name == 'p':
+                text += ' '.join(child.strings)
+                paragraph_counter += 1
+
+                if paragraph_counter == 2:
+                    break
+
+        # remove all references
+        text = re.sub('\[[0-9].+\]', text, '')
+
+        # remove last line if the last character is :
+        # note paragraphs end with line feed U+000A, thus it is technically the second last character
+        if text is not None and len(text) >= 2 and text[-2] == ':': 
+            last_period_index = text.rfind('.')
+            if last_period_index == -1:
+                text = ''
+            else:
+                text = text[:last_period_index + 1]
+
+        if text != '':
+            return text
+
+    return None
 
 from flask_cors import CORS
 CORS(app, origins=['http://127.0.0.1:3000'])
